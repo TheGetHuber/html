7f15a2282f9a3d57e2dc9ef345c107af9aa17037
TheGetHuber <reidolf.danila@gmail.com>
2023-01-08 01:05:42 +0300

    midnight commit
    overhaulf of New Game menu
    TODO: minecraft buttons template

--- js/minecraft/splash.js
+++ js/minecraft/splash.js
@@ -1,6 +1,7 @@
 let splashes = ["Garry's Mod!", "Flaxkikare!", "Now with Mingebags!", 
 "100% pure!", "Awesome!", "Now with CS:S content!", "Now without an observer!",
-"Sandbox!", "DarkRP!", "TTT!", "Peter Griffin!"]
+"Sandbox!", "DarkRP!", "TTT!", "Peter Griffin!", "Moyai", ":skull:",
+"41.7 GBs of addons!", "Now with FNAF saves!", "?"]
 
 window.onload = function(){
     let splash = document.querySelector(".titlesplash")
