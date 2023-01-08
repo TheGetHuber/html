55a5294268a3b1f1638f51667a36683c8bf22d07
TheGetHuber <reidolf.danila@gmail.com>
2023-01-08 13:18:06 +0300

    Major commit
    added Game Settings menu (placeholder)
    fixed some link bugs
    fixed minecraft button system's styles
    added some splashes

--- js/minecraft/splash.js
+++ js/minecraft/splash.js
@@ -1,7 +1,13 @@
-let splashes = ["Garry's Mod!", "Flaxkikare!", "Now with Mingebags!", 
-"100% pure!", "Awesome!", "Now with CS:S content!", "Now without an observer!",
-"Sandbox!", "DarkRP!", "TTT!", "Peter Griffin!", "Moyai", ":skull:",
-"41.7 GBs of addons!", "Now with FNAF saves!", "?"]
+let splashes = ["Garry's Mod!", "Flaxkikare!", "Now with Mingebags!",
+    "100% pure fun!", "Awesome!", "Now with CS:S content!", "Now without a black figure!",
+    "Sandbox!", "DarkRP!", "TTT!", "Murder!", "Melon racer!", "GM Tower!", "Peter Griffin!",
+    "Moyai", ":skull:", "41.7 GBs of addons!", "Now with FNAF saves!", "?", "Humorous!", 
+    "bloxwich", "Made by Garry Newman!", "Gabe Newell!", "Gaben!", "Powered by Source!", 
+    "gm_bigcity!", "Cough cough cough", "Without GlueLib!", "You have time to hide", "NonRP!", 
+    "Multiplayer!", "Meaty", "You need me", "Distorted trumpet!", "Now with ARG's!",
+    "Now with Lua!", "Wiremod!", "Now without Gman virus!", "Creepy!", "Door stuck!",
+    "Now without backdoors!", "Also try Team Fortress 2!", "Heavy is dead!?", "garry!", "Rabscuttle!",
+    "Facepunch!", "Missing texture!", "s&box!", "Roblox!", "M9K Weapons!", "Toybox!"]
 
 window.onload = function(){
     let splash = document.querySelector(".titlesplash")
